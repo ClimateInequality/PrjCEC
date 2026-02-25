831039742f1d054fc63c44e09ee64ddbf983817b
szkaifeng <32334635+szkaifeng@users.noreply.github.com>
2026-02-25 12:39:18 +0100

    Update decomp_1989t1991pop_2019t2021utci.R
    remove some commented out lines for clarity

--- R-script/run_3a_decompose/decomp_1989t1991pop_2019t2021utci.R
+++ R-script/run_3a_decompose/decomp_1989t1991pop_2019t2021utci.R
@@ -99,19 +99,6 @@ colnames(df_china_census_county_1990)[2:37] <-c(
 ##############################################
 # df_key_loc_china_coord2county_1990 ----
 
-# Should only need to do this once, to create ave file with long, lat
-
-locID20 <- read_csv("/Users/mlaghi/Dropbox/PIRE/team/marco_laghi/PrjCECReplicate/clean_data/df_era5_utci_china_2019_2021_hour.csv")
-coord20 <- read_csv("/Users/mlaghi/Documents/GitHub/PrjCEC/data/df_key_loc_china_coord2county_2020.csv")
-coord20 <- coord20 %>%
-  select(Long, Lat, location_id) %>%
-  left_join(locID20, by = location_id) %>%
-  select(-location_id)
-write_csv("/Users/mlaghi/Dropbox/PIRE/team/marco_laghi/PrjCECReplicate/clean_data/utci_ave_2019t2021_hour.csv")
-
-
-#return to Kai code
-
 loader1 <- load("/Users/mlaghi/Dropbox/DAEO_bigdata_prc/w_c_era5/utci/cim_annual_1940t2020/utci_china_Y2019_hourly.rda")
 ml1 <- get(loader1[1])
 loader2 <- load("/Users/mlaghi/Dropbox/DAEO_bigdata_prc/w_c_era5/utci/cim_annual_1940t2020/utci_china_Y2020_hourly.rda")
@@ -232,91 +219,7 @@ df_key_loc_china_coord2county_1990<-df_key_loc_china_coord2county_1990 %>%
 write.csv(df_era5_utci_china_1990,"/Users/mlaghi/Dropbox/PIRE/team/marco_laghi/PrjCECReplicate/clean_data/df_era5_utci_china_1990demo_2019t2021utci.csv",row.names = FALSE)
 
 
-# # df_key_demo_china_census_1990 ----
-# 
-# df_key_demo_china_census_1990 <- data.frame (
-#   year = "1990",
-#   popgrp_key= c("popgrp1","popgrp2","popgrp3","popgrp4","popgrp5",
-#                 "popgrp6","popgrp7","popgrp8","popgrp9","popgrp10",
-#                 "popgrp11","popgrp12","popgrp13","popgrp14","popgrp15",
-#                 "popgrp16","popgrp17","popgrp18","popgrp19","popgrp20",
-#                 "popgrp21","popgrp22","popgrp23","popgrp24", "popgrp25",
-#                 "popgrp26","popgrp27","popgrp28","popgrp29","popgrp30",
-#                 "popgrp31","popgrp32","popgrp33","popgrp34","popgrp35","popgrp36"),
-#   gender = c("male","female","male","female","male","female","male","female","male","female",
-#              "male","female","male","female","male","female","male","female","male","female",
-#              "male","female","male","female","male","female","male","female","male","female",
-#              "male","female","male","female","male","female"),
-#   age_group=c("00_04","00_04","05_09","05_09","10_14","10_14","15_19","15_19",
-#               "20_24","20_24","25_29","25_29","30_34","30_34","35_39","35_39",
-#               "40_44","40_44","45_49","45_49","50_54","50_54","55_59","55_59",
-#               "60_64","60_64","65_69","65_69","70_74","70_74","75_79","75_79",
-#               "80_84","80_84","85_85","85_85"),
-#   all_groups = 1,
-#   age_group_m3 = c("0_14","0_14","0_14","0_14","0_14","0_14","15_59","15_59",
-#                    "15_59","15_59","15_59","15_59","15_59","15_59","15_59","15_59",
-#                    "15_59","15_59","15_59","15_59","15_59","15_59","15_59","15_59",
-#                    "60_85","60_85","60_85","60_85","60_85","60_85","60_85","60_85",
-#                    "60_85","60_85","60_85","60_85"),
-#   label = c(
-#     "Total Males at 0-4 Age",
-#     "Total Females at 0-4 Age",
-#     "Total Males at 5-9 Age",
-#     "Total Females at 5-9 Age",
-#     "Total Males at 10-14 Age",
-#     "Total Females at 10-14 Age",
-#     "Total Males at 15-19 Age",
-#     "Total Females at 15-19 Age",
-#     "Total Males at 20-24 Age",
-#     "Total Females at 20-24 Age",
-#     "Total Males at 25-29 Age",
-#     "Total Females at 25-29 Age",
-#     "Total Males at 30-34 Age",
-#     "Total Females at 30-34 Age",
-#     "Total Males at 35-39 Age",
-#     "Total Females at 35-39 Age",
-#     "Total Males at 40-44 Age",
-#     "Total Females at 40-44 Age",
-#     "Total Males at 45-49 Age",
-#     "Total Females at 45-49 Age",
-#     "Total Males at 50-54 Age",
-#     "Total Females at 50-54 Age",
-#     "Total Males at 55-59 Age",
-#     "Total Females at 55-59 Age",
-#     "Total Males at 60-64 Age",
-#     "Total Females at 60-64 Age",
-#     "Total Males at 65-69 Age",
-#     "Total Females at 65-69 Age",
-#     "Total Males at 70-74 Age",
-#     "Total Females at 70-74 Age",
-#     "Total Males at 75-79 Age",
-#     "Total Females at 75-79 Age",
-#     "Total Males at 80-84 Age",
-#     "Total Females at 80-84 Age",
-#     "Total Males at 85 Age and over",
-#     "Total Females at 85 Age and over"
-#   ))
-# 
-# #write.csv(df_key_demo_china_census_1990,"C:/Users/Kaifs/OneDrive/Documents/dropbox_penn/Dropbox/PIRE/team/kai_feng/clean_data/df_key_demo_china_census_1990.csv",row.names = FALSE)
-# 
-# 
-# # df_key_loc_china_county2province_1990 ----
-# df_key_loc_china_county2province_1990 <- st_read(shapefile1990_path, geometry_column = "geometry") %>%
-#   rename(GBCounty=CNTYGB,
-#          Prov_En = EPROV) %>% 
-#   st_drop_geometry() %>%
-#   subset(GBCounty != "469037" & GBCounty != "469038" & 
-#            GBCounty != "469039" & GBCounty != "710000" &
-#            GBCounty != "810000" & GBCounty != "820000") %>%
-#   select("GBCounty","Prov_En") %>%
-#   mutate(all_locations=1)
-# 
-# #for county 420822, "shangyang gongguanju", the EPROV was labeled as "Nation". It should be labeled as "Hubei". 
-# #it is a prison unit in Hubei. We relabeled it as "Hubei".
-# 
-# 
-# #write.csv(df_key_loc_china_county2province_1990,"C:/Users/Kaifs/OneDrive/Documents/dropbox_penn/Dropbox/PIRE/team/kai_feng/clean_data/df_key_loc_china_county2province_1990.csv")
-# 
+
 
 
 

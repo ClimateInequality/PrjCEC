7d23670e06e119b4a325176c1c66eb701fd4022c
szkaifeng <32334635+szkaifeng@users.noreply.github.com>
2026-02-25 12:25:51 +0100

    Update decomp_2019t2021pop_1989t1991utci.R
    change final product name to df_era5_utci_china_2020demo_1989t1991utci.csv

--- R-script/run_3a_decompose/decomp_2019t2021pop_1989t1991utci.R
+++ R-script/run_3a_decompose/decomp_2019t2021pop_1989t1991utci.R
@@ -287,6 +287,7 @@ df_key_loc_china_coord2county_2020<-df_key_loc_china_coord2county_2020 %>%
 
 
 #write.csv(df_key_loc_china_coord2county_2020,"C:/Users/Kaifs/OneDrive/Documents/dropbox_penn/Dropbox/PIRE/team/kai_feng/clean_data/df_key_loc_china_coord2county_2020.csv",row.names = FALSE)
-write.csv(df_era5_utci_china_1990,"/Users/mlaghi/Dropbox/PIRE/team/marco_laghi/PrjCECReplicate/clean_data/df_era5_utci_china_2020_hour_decomp1989t1991.csv",row.names = FALSE)
+write.csv(df_era5_utci_china_1990,"/Users/mlaghi/Dropbox/PIRE/team/marco_laghi/PrjCECReplicate/clean_data/df_era5_utci_china_2020demo_1989t1991utci.csv",row.names = FALSE)
+
 
 

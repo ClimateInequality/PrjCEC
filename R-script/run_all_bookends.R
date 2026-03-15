0c291bc89a02c7c78e7fe1a062aa1617166e2014
Fan Wang <wangfanbsg75@live.com>
2026-03-15 06:24:51 -0500

    `data-res` Triple year update #49 results, new decompose #52 results
    - Second time generating heat exposure stats for triple year
    - Turns out all results last time were wrong because were using 1991 three times and 2021 three times
    - Now input file is actual three year exposure information, all hours 89 to 91 and all hours 19 to 21
    - New decomposition pop vs heat analysis

--- R-script/run_all_bookends.R
+++ R-script/run_all_bookends.R
@@ -1,13 +1,11 @@
+# 2026-03-10 12:56:50, restart because commented out `source("R-script/run_1c_mean_child_seasons/ffs_pirecec_nonwinter_bookends_convert.R")`
 ar_it_results_set <- c(
-    11, 12, 13,
+    13,
     21,
     31, 32,
     41, 42, 
     51
 )
-ar_it_results_set <- c(13)
-ar_it_results_set <- c(41, 42)
-
 if (11 %in% ar_it_results_set) {
     # all 24 hours
     source("R-script/run_1a_mean_child_all24/ffs_pirecec_demo_1989t1991_all24.R")
@@ -20,7 +18,7 @@ if (12 %in% ar_it_results_set) {
     source("R-script/run_1b_mean_child_6t22/ffs_pirecec_demo_2019t2021_6t22.R")
 }
 if (13 %in% ar_it_results_set) {
-    # source("R-script/run_1c_mean_child_seasons/ffs_pirecec_nonwinter_bookends_convert.R")
+    source("R-script/run_1c_mean_child_seasons/ffs_pirecec_nonwinter_bookends_convert.R")
     # April to September
     source("R-script/run_1c_mean_child_seasons/ffs_pirecec_demo_1989t1991_apr2sep.R")
     source("R-script/run_1c_mean_child_seasons/ffs_pirecec_demo_2019t2021_apr2sep.R")
@@ -34,12 +32,12 @@ if (21 %in% ar_it_results_set) {
     # source("R-script/run_2a_atrisk_child/ffs_pirecec_demo_jntdist_atrisk.R")
 }
 if (31 %in% ar_it_results_set) {
-    source("R-script/run_3a_decompose/ffs_pirecec_demo_1990pop_2020utci_par.R")
-    source("R-script/run_3a_decompose/ffs_pirecec_demo_2020pop_1990utci_par.R")
+    source("R-script/run_3a_decompose/ffs_pirecec_demo_1990pop_2019t2021utci_par.R")
+    source("R-script/run_3a_decompose/ffs_pirecec_demo_2020pop_1989t1991utci_par.R")
 }
 if (32 %in% ar_it_results_set) {
-    source("R-script/run_3b_decompose_regional/ffs_pirecec_demo_loc_region_1990pop_2020utci_par.R")
-    source("R-script/run_3b_decompose_regional/ffs_pirecec_demo_loc_region_2020pop_1990utci_par.R")
+    source("R-script/run_3b_decompose_regional/ffs_pirecec_demo_loc_region_1990pop_2019t2021utci_par.R")
+    source("R-script/run_3b_decompose_regional/ffs_pirecec_demo_loc_region_2020pop_1989t1991utci_par.R")
 }
 if (41 %in% ar_it_results_set) {
     # regional results, 24 hours
